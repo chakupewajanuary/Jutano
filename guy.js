3c44442d50d03b419f95698c9ee5b359a055e6cb
chakupewa <chakupewajanuary38@gamil.com>
2024-08-27 12:39:49 +0300

    updated code commited 27th august

--- guy.js
+++ guy.js
@@ -1,3 +1,30 @@
 console.log("mmama");
 console.log("yaya");
 alert("ugonjwa");
+// from html file dom(document object model)
+const form=document.querySelector('.form');
+const email=document.getElementById('email');
+const phone=document.getElementById('phone');
+const Name=document.querySelector('#name');
+const massage=document.getElementById('tst');
+const submit=document.getElementById('btn');
+const msg=document.querySelector('.msg');
+
+form.addEventListener('submit',onsubmit)
+
+function onsubmit(e){
+    e.preventDefault();
+    
+    if(Name.value===''||email.value===''){
+        msg.innerHTML("please fill all the fields");
+        setTimeout(()=>msg.remove(),3000)
+
+    }
+    else{
+        console.log(`this entered name ${Name.value}`)
+        console.log(`this entered name ${email.value}`)
+        console.log(`this entered name ${phone.value}`)
+        console.log(`this entered name ${massage.value}`)
+    }
+
+}
